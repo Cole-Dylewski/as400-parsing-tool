eed83bc705820d933a40c2ee4023a9d249d8496c
Dylewski, Cole (ES) <Cole.Dylewski@adp.com>
2022-08-25 16:02:59 -0400

    added city zip to dict

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ for i in range(15, 150):
                     tbd = line[0:36]
                     #print("Unknown data/TBD: ", tbd)
             if lineCounter == 3:
-                    cityStateZip = line[1:27]
+                    recordDict['cityStateZip'] = line[1:27]
                     #print("City, State, Zip: ", cityStateZip)
                     pens = line[27:33]
                     #print("Pens: ", pens)
